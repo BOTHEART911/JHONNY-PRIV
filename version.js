fa0c9228067b6e04b1a2b6011bb1a245d8d64737
Oscar Polania <opolania11@gmail.com>
2026-08-01 21:59:31 -0500

    Update version.js

--- version.js
+++ version.js
@@ -4,4 +4,4 @@
  * si cambia, limpia caches y recarga automáticamente en todos
  * los dispositivos. También alimenta el texto "Versión X".
  * ============================================================ */
-var APP_VERSION = "2026.08.01.4";
+var APP_VERSION = "2026.08.01.5";
